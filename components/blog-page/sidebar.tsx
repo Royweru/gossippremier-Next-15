0ea517410fc54b4d422c99ce6a1e2e4abdbd8f29
Royweru <royezi17@gmail.com>
2025-03-02 12:55:17 +0300

    Made changes of imports in the navbar

--- components/blog-page/sidebar.tsx
+++ components/blog-page/sidebar.tsx
@@ -1,4 +1,4 @@
-import { urlFor } from "@/app/(routes)/blogs/[slug]/page";
+import { urlFor } from "@/lib/sanity";
 import { PostType } from "@/types";
 import Image from "next/image";
 import React from "react";
