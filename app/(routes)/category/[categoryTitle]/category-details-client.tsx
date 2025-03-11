6d3e8242572cb69fe6649a31254b60aa22e02df9
Royweru <royezi17@gmail.com>
2025-03-11 08:23:44 +0300

    Created the contact page

--- app/(routes)/category/[categoryTitle]/category-details-client.tsx
+++ app/(routes)/category/[categoryTitle]/category-details-client.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 "use client"
+import { urlFor } from '@/lib/imageBuilder'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
@@ -7,13 +8,26 @@ import React from 'react'
 const CategoryDetailsClient = ({posts}:{
     posts:any[]
 }) => {
+   const categoryTitle = posts[0]?.category?.title ||"ENTERTAINMENT"
   return (
     <div className=" w-full space-y-2">
-    <div className=" overflow-hidden border-b border-gray-900 flex flex-col w-full  last:border-0 relative sm:flex-row">
+       
+      {/* Header */}
+      <header className=" py-3 border-b">
+        <div className="max-w-6xl mx-auto px-4">
+          <h1 className="text-5xl font-bold text-gray-900">Category</h1>
+          <div className="flex items-center mt-4 text-sm text-gray-600">
+            <Link href={'/'}>Home</Link>
+            <span className="mx-2">&gt;</span>
+            <span>Category {categoryTitle}</span>
+          </div>
+        </div>
+      </header>
+    <div className=" overflow-hidden border-b border-gray-900 flex flex-col w-full  last:border-0 relative sm:flex-row lg:py-4 md:py-3 py-2 ">
         <div className='sm:w-1/2 md:w-1/3 w-full'>
         <div className="relative sm:h-full h-[300px] w-full rounded-xl">
         <Image
-          src="/health2.jpg"
+          src={posts[0]?.mainImage&& urlFor(posts[0]?.mainImage)?.url()||'/lifestyle2.jpg'}
           alt="Palm trees at sunset"
           fill
           className="transition-transform  rounded-xl
@@ -24,12 +38,12 @@ const CategoryDetailsClient = ({posts}:{
      
       <div className="px-4 relative sm:w-1/2 md:w-2/3 w-full py-3">
         <div className="uppercase tracking-wide text-sm text-pink-500 font-semibold mb-2">
-         {posts[1]?.category?.title||"ENTERTAINMENT"} 
+         {categoryTitle} 
         
         </div>
-        <Link href="/lifestyle/uk-outdoor-restaurants" className="block">
+        <Link href="/blogs" className="block">
           <h2 className="text-lg md:text-xl font-bold text-gray-900 hover:text-pink-500 transition-colors">
-            The UK&apos;s Best Outdoor Restaurants and Terraces
+            {posts[0]?.title||"The most controversial people on the internet today"}
           </h2>
         </Link>
         <p className="mt-3 text-gray-600 md:text-sm text-xs font-montserrat">
@@ -44,11 +58,11 @@ const CategoryDetailsClient = ({posts}:{
         </div>
       </div>
     </div>
-    <div className=" overflow-hidden border-b border-gray-900 flex flex-col w-full  last:border-0 relative sm:flex-row">
+    <div className=" overflow-hidden border-b border-gray-900 flex flex-col w-full  last:border-0 relative sm:flex-row lg:py-4 md:py-3 py-2">
         <div className='sm:w-1/2 md:w-1/3 w-full'>
         <div className="relative sm:h-full h-[300px] w-full rounded-xl">
         <Image
-          src="/culture1.jpg"
+          src={posts[1]?.mainImage&&(urlFor(posts[1]?.mainImage)?.url())||'/culture1.jpg'}
           alt="Palm trees at sunset"
           fill
           className="transition-transform  rounded-xl
@@ -59,7 +73,7 @@ const CategoryDetailsClient = ({posts}:{
      
       <div className="px-4 relative sm:w-1/2 md:w-2/3 w-full py-3">
         <div className="uppercase tracking-wide text-sm text-pink-500 font-semibold mb-2">
-         {posts[0]?.category?.title||"ENTERTAINMENT"} 
+         {categoryTitle} 
         
         </div>
         <Link href="/lifestyle/uk-outdoor-restaurants" className="block">
